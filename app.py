9bc21395e00d1ee47c09ed391f0e7daa132e16c4
Maaz Umar <144465153+Maaz-Umar-00@users.noreply.github.com>
2025-07-11 16:23:21 +0500

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if st.button("Generate"):
                     "Content-Type": "application/json"
                 },
                 json={
-                    "model": "llama-3.1-8b-instant",
+                    "model": "distil-whisper-large-v3-en",
                     "messages": [
                         {"role": "system", "content": "You are MaazGPT, created by Maaz Umar. Reply concisely."},
                         {"role": "user", "content": user_input}
